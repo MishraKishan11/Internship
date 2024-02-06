0ac92d37b91e05bb6a61861b563b76b5573e4442
MishraKishan11 <144704956+MishraKishan11@users.noreply.github.com>
2024-02-06 19:07:15 +0530

    Add files via upload

---
+++ CountManager.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mcq_app;
+
+/**
+ *
+ * @author kanha
+ */
+public class CountManager {
+    private static int count = 0;
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int newCount) {
+        count = newCount;
+    }
+}
+
